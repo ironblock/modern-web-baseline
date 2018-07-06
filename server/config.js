c63b40d618ab63303a7d642784627001598a8902
Corey Vixie <corey.vixie@gmail.com>
2018-07-06 14:48:22 -0700

    Webserver setup

---
+++ server/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    protocol: 'http',
+    host: '0.0.0.0',
+    port: 31337
+  },
+  production: {
+    protocol: 'https',
+    host: '0.0.0.0',
+    port: 80
+  },
+}
---
+++ server/index.js
@@ -0,0 +1,76 @@
+// @flow
+// WEBSERVER
+// =============================================================================
+
+// Remove BROWSER flag to ensure all serverside logic is properly excecuted
+delete process.env.BROWSER;
+
+// Display unhandled promise rejection tracebacks in node console
+process.on('unhandledRejection', rejection => console.error(rejection));
+
+const path = require('path');
+
+const Express = require('express');
+const webpack = require('webpack');
+const devMiddleware = require('webpack-dev-middleware');
+const hotMiddleware = require('webpack-hot-middleware');
+const hotServerMiddleware = require('webpack-hot-server-middleware');
+
+const config = require('./config');
+let port;
+
+const app = new Express();
+
+// In the case of NODE_ENV=staging or undefined default to production.
+const nodeEnv = process.env.NODE_ENV === 'development'
+  ? 'development'
+  : 'production';
+
+console.info(`Starting webserver with NODE_ENV=${nodeEnv}`);
+
+// EXPRESS CONFIGURATION
+// =====================
+app.use('/public', Express.static(path.join(__dirname, '..', 'public')));
+
+// LIVE DEVELOPMENT MODE
+if (nodeEnv === 'development') {
+  const webpackConfig = require('../webpack/development.config');
+  const rootCompiler = webpack(webpackConfig);
+
+  port = config.development.port;
+
+  app.use(devMiddleware(rootCompiler, {
+    publicPath: webpackConfig[0].output.publicPath,
+    serverSideRender: true,
+    stats: 'errors-only',
+  }));
+
+  app.use(hotMiddleware(rootCompiler.compilers.find(
+    compiler => compiler.name === 'client')
+  ));
+  app.use(hotServerMiddleware(rootCompiler));
+} else if (nodeEnv === 'production') {
+  const stats = require('../dist/stats.generated.json');
+  const serverRenderer = require('../dist/server').default;
+
+  port = config.development.port;
+
+  app.use('/assets/', Express.static(path.join(__dirname, '..', 'dist', 'assets')));
+  app.use(serverRenderer(stats));
+  app.use((err, req, res) => {
+    console.error(err.stack);
+    res.status(500).send('Internal server error: 500');
+  });
+} else {
+  throw new Error(`A mode of "${nodeEnv}" is not supported`);
+}
+
+app.listen(port, (error) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  if (nodeEnv === 'development') {
+    console.info('Webpack build running, please wait...');
+  }
+});
