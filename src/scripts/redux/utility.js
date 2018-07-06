22cad716f9da05cd947af2b0af0d17face49e313
Corey Vixie <corey.vixie@gmail.com>
2018-07-06 14:50:15 -0700

    Add custom redux utilities

---
+++ src/scripts/redux/utility.js
@@ -0,0 +1,153 @@
+// @flow
+// API - UTILITIES
+// =============================================================================
+// General purpose utilities required by the generated output files.
+
+// FLUX STANDARD ACTIONS
+// These may be brought in by something like flow-typed in the future. For the
+// time being, this is a relatively faithful conversion of the TypeScript
+// equivalents defined in https://github.com/redux-utilities/flux-standard-action/blob/master/src/index.d.ts
+
+/* eslint-disable no-use-before-define */
+export type ActionType = string;
+export type NormalFSA<Payload: mixed | Error = typeof undefined, Meta: mixed = typeof undefined> = {|
+  type: ActionType,
+  payload: Payload,
+  error: boolean,
+  meta: Meta
+|};
+export type FluxStandardAction<Payload, Meta> = NormalFSA<Payload, Meta>;
+export type FSA<Payload, Meta> = NormalFSA<Payload, Meta>;
+
+// JSON TYPES
+export type PrimitiveType = null | string | boolean | number;
+export type JSONObject = ({ [key: string]: PrimitiveType | JSONObject | JSONArray | void } & { $call?: void });
+export type JSONArray = Array<PrimitiveType> | Array<JSONObject> | Array<JSONArray>;
+export type JSONType = JSONObject | JSONArray | PrimitiveType;
+
+// SWAGGER GEN JS UTILITY TYPES
+export type ApiEndpointState = {
+  success: JSONType | null,
+  failure: JSONType | null,
+  timeout: JSONType | null,
+  mistake: JSONType | null,
+  isFetching: boolean,
+  lastUpdate: number | null,
+  lastResult: 'success' | 'failure' | 'timeout' | 'mistake' | null,
+};
+export type KeyedCollectionState =
+  & ApiEndpointState
+  & { collection: { [string]: JSONObject } };
+
+export type ReducerState<Name: string> =
+  & { [string]: ApiEndpointState }
+  & { [Name]: ApiEndpointState };
+export type KeyedReducerState<Name: string> =
+  & { [string]: KeyedCollectionState }
+  & { [Name]: KeyedCollectionState };
+
+export const handleRequest = <Name: string, State: ReducerState<Name>>(
+  name: Name,
+  state: State,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialEndpointState,
+      ...state[name],
+      isFetching: true,
+    },
+  }
+);
+
+export const handleSuccess = <Name: string, State: ReducerState<Name>>(
+  name: Name,
+  state: State,
+  action: FSA<*, *>,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialEndpointState,
+      ...state[name],
+      success: action.payload || null,
+      isFetching: false,
+      lastUpdate: Date.now(),
+      lastResult: 'success',
+    },
+  }
+);
+
+export const handleSuccessKeyed = <Name: string, State: KeyedReducerState<Name>>(
+  name: Name,
+  state: State,
+  action: FSA<*, *>,
+  key: string,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialKeyedCollectionState,
+      ...state[name],
+      success: action.payload || null,
+      isFetching: false,
+      lastUpdate: Date.now(),
+      lastResult: 'success',
+      collection: handleKeyedCollection(key, state[name].collection, action.payload),
+    },
+  }
+);
+
+export const handleFailure = <Name: string, State: ReducerState<Name>>(
+    name: Name,
+    state: State,
+    action: FSA<*, *>,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialEndpointState,
+      ...state[name],
+      failure: action.payload || null,
+      isFetching: false,
+      lastUpdate: Date.now(),
+      lastResult: 'failure',
+    },
+  }
+);
+
+export const handleTimeout = <Name: string, State: ReducerState<Name>>(
+    name: Name,
+    state: State,
+    action: FSA<*, *>,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialEndpointState,
+      ...state[name],
+      timeout: action.payload || null,
+      isFetching: false,
+      lastUpdate: Date.now(),
+      lastResult: 'timeout',
+    },
+  }
+);
+
+export const handleMistake = <Name: string, State: ReducerState<Name>>(
+    name: Name,
+    state: State,
+    action: FSA<*, *>,
+  ): State => (
+  {
+    ...state,
+    [name]: {
+      ...initialEndpointState,
+      ...state[name],
+      mistake: action.payload || null,
+      isFetching: false,
+      lastUpdate: Date.now(),
+      lastResult: 'mistake',
+    },
+  }
+);
